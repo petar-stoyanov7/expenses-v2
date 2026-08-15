7f3d4c6222034150ff66cb3bcc62ae3bacce92b3
Petar Stoyanov <petar.stoyanov@gmail.com>
2026-08-15 09:08:10 +0300

    Added .env variables to webpack so it can be consumed by FE.

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+require('dotenv-flow').config();
+
 const Encore = require('@symfony/webpack-encore');
 
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
@@ -69,6 +71,30 @@ Encore
     // uncomment if you're having problems with a jQuery plugin
     //.autoProvidejQuery()
     .enableReactPreset()
+    .configureDefinePlugin((options) => {
+        options['process.env'] = {
+            SERVER_URL: JSON.stringify(process.env.SERVER_URL),
+            HASH: JSON.stringify(process.env.HASH),
+            REGISTER_PATH: JSON.stringify(process.env.REGISTER_PATH),
+            LOGIN_PATH: JSON.stringify(process.env.LOGIN_PATH),
+            USER_EDIT_PATH: JSON.stringify(process.env.USER_EDIT_PATH),
+            GET_USER_PATH: JSON.stringify(process.env.GET_USER_PATH),
+            GET_CAR_PATH: JSON.stringify(process.env.GET_CAR_PATH),
+            CREATE_CAR_PATH: JSON.stringify(process.env.CREATE_CAR_PATH),
+            EDIT_CAR_PATH: JSON.stringify(process.env.EDIT_CAR_PATH),
+            DELETE_CAR_PATH: JSON.stringify(process.env.DELETE_CAR_PATH),
+            GET_USER_EXPENSES_PATH: JSON.stringify(process.env.GET_USER_EXPENSES_PATH),
+            GET_CAR_EXPENSES_PATH: JSON.stringify(process.env.GET_CAR_EXPENSES_PATH),
+            GET_FUELS_PATH: JSON.stringify(process.env.GET_FUELS_PATH),
+            GET_EXPENSE_TYPES_PATH: JSON.stringify(process.env.GET_EXPENSE_TYPES_PATH),
+            ADD_EXPENSE_PATH: JSON.stringify(process.env.ADD_EXPENSE_PATH),
+            IMPORT_EXPENSES_PATH: JSON.stringify(process.env.IMPORT_EXPENSES_PATH),
+            GET_OVERALL_PATH: JSON.stringify(process.env.GET_OVERALL_PATH),
+            GET_LAST_FIVE_PATH: JSON.stringify(process.env.GET_LAST_FIVE_PATH),
+            EDIT_EXPENSE_PATH: JSON.stringify(process.env.EDIT_EXPENSE_PATH),
+            DELETE_EXPENSE_PATH: JSON.stringify(process.env.DELETE_EXPENSE_PATH)
+        }
+    })
 ;
 
 module.exports = Encore.getWebpackConfig();
